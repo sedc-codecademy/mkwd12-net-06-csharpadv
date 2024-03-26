f17df4f4d7ee7132f228fb65851952a6082ac16b
tstojanovska <stojanovskatanja03@gmail.com>
2024-03-26 20:43:20 +0100

    Class 01

---
+++ g7/Class01/Code/Task01/Program.cs
@@ -0,0 +1,32 @@
+﻿List<string> words = new List<string>();
+
+string input = string.Empty;
+do
+{
+    Console.WriteLine("Please enter a word:");
+    string word = Console.ReadLine();
+
+    //todo validation if the input is empty
+
+    words.Add(word);
+
+    Console.WriteLine("If you want to enter another word press any other key then X");
+    input = Console.ReadLine();
+} while (input != "x" && input != "X");
+
+Console.WriteLine("Please enter the text:");
+string text = Console.ReadLine();
+
+List<string> textWords = text.Split(" ").ToList();
+
+foreach (string word in words)
+{
+    //int wordCount = textWords
+    //    .Where(x => string.Equals(x, word, StringComparison.InvariantCultureIgnoreCase)).ToList().Count;
+
+    int wordCount = textWords.Count(x => string.Equals(x, word, StringComparison.InvariantCultureIgnoreCase));
+    //int wordCount = textWords.Count(x => x.ToLower() == word.ToLower());
+
+    //nice to have - Dictionary -> key of type string (word), value of type int (count)
+    Console.WriteLine($"{word}: {wordCount}");
+}
---
+++ g7/Class01/Code/Task03/Enums/ResultEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace Task03.Enums;
+
+public enum ResultEnum
+{
+    PlayerWins = 1,
+    ComputerWins,
+    Draw
+}
