8a500511afc75128b8a8486d0741d78f04c8e4fd
tstojanovska <stojanovskatanja03@gmail.com>
2024-03-28 20:49:14 +0100

    Class 02

---
+++ g7/Class02/Code/QinshiftAcademy.Class02.Exercise01/QinshiftAcademy.Class02.Exercise01.Domain/Models/Student.cs
@@ -0,0 +1,49 @@
+﻿using QinshiftAcademy.Class02.Exercise01.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QinshiftAcademy.Class02.Exercise01.Domain.Models
+{
+    public class Student : User, IStudent
+    {
+        public List<int> Grades { get; set; }
+
+        public Student(string firstName, string lastName, string userName, string password, List<int> grades)
+            : base(firstName, lastName, userName, password)
+        {
+            Grades = grades != null ? grades : new List<int>();
+        }
+
+        //from the abstract class
+        public override void PrintUser()
+        {
+            Console.WriteLine($"Student {FirstName} {LastName} has the following grades:");
+            foreach (int grade in Grades)
+            {
+                Console.Write($"{grade} ");
+
+            }
+
+            Console.WriteLine();
+        }
+
+        //from the interface
+        public double GetAverageGrade()
+        {
+            int sum = 0;
+
+            foreach (int grade in Grades)
+            {
+                sum += grade;
+            }
+
+            //we cast the value of the variable sum to double, which means we say that it's type
+            //should be seen as double in this case
+            //we do this to avoid division of two integers
+            return Grades.Count > 0 ? (double)sum / Grades.Count : 0;
+        }
+    }
+}
---
+++ g7/Class02/Code/QinshiftAcademy.Class02/QinshiftAcademy.Class02.AbstractClasses/Interfaces/ISoftwareDeveloper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QinshiftAcademy.Class02.AbstractClasses.Interfaces
+{
+    //what ever class inherits from this interface will have to provide implementation for the Code method
+    //or just contain the Code method signature, but will be marked as abstract
+    public interface ISoftwareDeveloper
+    {
+        void Code();
+    }
+}
