1d60956264f3f4c3e67858ea61dd96349025b18d
tijanastojanovska <stojanovska_tijana@outlook.com>
2024-03-28 20:51:20 +0100

    Class 02

---
+++ g6/Class 02/Code/AbstractClassesAndInterface.Domain/Models/DevOpsEngineer.cs
@@ -0,0 +1,45 @@
+﻿using AbstractClassesAndInterface.Domain.Interfaces;
+
+namespace AbstractClassesAndInterface.Domain.Models
+{
+    public class DevOpsEngineer : Person, IDevOpsEngineer, IDeveloper
+    {
+        public bool IsAzureCertificated { get; set; }
+        public bool IsAWSCertificated { get; set; }
+
+        public DevOpsEngineer(string fullName, int age, string address, long phoneNumer, 
+            bool isAzureCertificated, bool isAWSCertificated) : base(fullName, age, address, phoneNumer)
+        {
+            IsAzureCertificated = isAzureCertificated;
+            IsAWSCertificated = isAWSCertificated;
+        }
+
+        public override string GetProfessionalInfo()
+        {
+            string info = $"{FullName}";
+            info += IsAzureCertificated ? " has Azure cerificate. " : " does not have an Azure certificate";
+            info += IsAWSCertificated ? " has AWS cerificate " : " does not have an AWS certificate ";
+            return info ;
+        }
+
+        public bool CheckInfrastucture(int status)
+        {
+            List<int> okStatuses = new List<int> { 200, 202, 204 };
+            return okStatuses.Contains(status); 
+        }
+
+        //each class can implement the methods differently, depending on the logic in that class
+        public void Code()
+        {
+            if (IsAzureCertificated)
+            {
+                Console.WriteLine("Writing code for Azure portal service");
+            }
+
+            if (IsAWSCertificated)
+            {
+                Console.WriteLine("Writing code for AWS Portal services");
+            }
+        }
+    }
+}
---
+++ g6/Class 02/Code/AbstractClassesAndInterface.Domain/Models/Person.cs
@@ -0,0 +1,45 @@
+﻿using AbstractClassesAndInterface.Domain.Interfaces;
+
+namespace AbstractClassesAndInterface.Domain.Models
+{
+    public abstract class Person : IPerson
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public long PhoneNumber { get; set; }
+
+        public Person()
+        {
+
+        }
+        //we don't use the person constructor to create instances of Person, but we use it in the derived classes when we call the base constructor
+        public Person(string fullName, int age, string address, long phoneNumber)
+        {
+            FullName = fullName;
+            Age = age;
+            Address = address;
+            PhoneNumber = phoneNumber;
+        }
+
+        //this method does not have an implementation in this (base) class. All classes that inherit from this class
+        //MUST provide an implementation for this method
+        public abstract string GetProfessionalInfo();
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"{FullName} : {PhoneNumber}");
+        }
+
+        //must implement them because Person implements IPerson interface
+        public void Greet()
+        {
+            Console.WriteLine($"Hello from {FullName}");
+        }
+
+        public void SendGift(string nameOfGift)
+        {
+            Console.WriteLine($"Sending {nameOfGift} from {Address}");
+        }
+    }
+}
