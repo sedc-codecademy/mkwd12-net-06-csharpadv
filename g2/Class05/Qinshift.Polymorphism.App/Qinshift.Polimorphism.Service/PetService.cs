b5ba5f8978c07804863c960ebcceb03e05f2d242
Danilo Borozan <daniloborozan07@gmail.com>
2024-04-06 09:56:05 +0200

    add code

---
+++ g2/Class05/Qinshift.Polymorphism.App/Qinshift.Polimorphism.Service/PetService.cs
@@ -0,0 +1,63 @@
+﻿using Qinshift.Polymorphism.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qinshift.Polymorphism.Service
+{
+    public class PetService
+    {
+        public void PetStatus(Dog dog, string ownerName)
+        {
+            Console.WriteLine($"Hey there {ownerName}");
+            if (dog.IsGoodBoi)
+            {
+                Console.WriteLine("This dog is a good boi!");
+            }
+            else
+            {
+                Console.WriteLine("This dog is a not good boi!");
+            }
+        }
+
+        public void PetStatus(string ownerName, Dog dog)
+        {
+            Console.WriteLine($"Hey there {ownerName}");
+            if (dog.IsGoodBoi)
+            {
+                Console.WriteLine("This dog is a good boi!");
+            }
+            else
+            {
+                Console.WriteLine("This dog is a not good boi!");
+            }
+        }
+
+        public void PetStatus(Cat cat, string ownerName)
+        {
+            Console.WriteLine($"Hey there {ownerName}");
+            if (cat.IsLazy)
+            {
+                Console.WriteLine("This cat is a very lazy!");
+            }
+            else
+            {
+                Console.WriteLine("This cat is a not lazy!");
+            }
+        }
+
+        public void PetStatus(Cat cat)
+        {
+            if (cat.IsLazy)
+            {
+                Console.WriteLine("This cat is a very lazy!");
+            }
+            else
+            {
+                Console.WriteLine("This cat is a not lazy!");
+            }
+        }
+    }
+}
