e7dde3e4cffadd5b7c2ec88406cf87edaca5d656
Danilo Borozan <daniloborozan07@gmail.com>
2024-04-06 12:49:45 +0200

    code

---
+++ g2/Class05/TaxiManager9000/TaxiManager9000.Domain/Enums/Shift.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiManager9000.Domain.Enums
+{
+    public enum Shift
+    {
+        Morning = 1,
+        Afternoon = 2,
+        Evening = 3
+    }
+}
