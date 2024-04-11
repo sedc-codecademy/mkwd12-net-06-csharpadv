98b39f765034d13b99690a64c131d2c46d0be798
Danilo Borozan <daniloborozan07@gmail.com>
2024-04-11 21:40:52 +0200

    code

---
+++ g2/Class07/Qinshift.Class07/Qinshift.Class07.Delegate/Program.cs
@@ -0,0 +1,49 @@
+﻿
+#region Methods
+void SayHello(string name)
+{
+    Console.WriteLine($"Hello {name}");
+}
+void SayBye(string name)
+{
+    Console.WriteLine($"Bye bye {name}");
+}
+void SayWhatever(string whatever, SayDelegate sayDel)
+{
+    Console.Write($"Says: ");
+    sayDel(whatever);
+}
+void Number(int num1, int num2, NumberDelegate numberDelegate)
+{
+    Console.WriteLine($"The result is: {numberDelegate(num1,num2)}");
+}
+#endregion
+
+SayDelegate del = new SayDelegate(SayHello);
+SayDelegate bye = new SayDelegate(SayBye);
+SayDelegate wow = new SayDelegate(x => Console.WriteLine($"{x} wow!!!!"));
+
+//Using the delegate
+del("Bob");
+bye("Jill");
+wow("Delegate");
+
+Console.WriteLine("============================");
+SayWhatever("Bla bla", SayBye);
+SayWhatever("Test test", SayHello);
+SayWhatever("Greg", x => Console.WriteLine($"{x} proba."));
+Console.WriteLine("============================");
+
+Number(2,3,(x,y)=> x+y);
+Number(2,3,(x,y)=> x-y);
+Number(2,3,(x,y)=> x*y);
+Number(9,3,(x,y)=> x/y);
+Number(2,5,(x,y)=>
+{
+    if (x >= y) return x;
+    return y;
+});
+
+delegate void SayDelegate(string something);
+delegate int NumberDelegate(int num1, int num2);
+
---
+++ g2/Class07/Qinshift.Class07/Qinshift.Events.App/Program.cs
@@ -0,0 +1,35 @@
+﻿using Qinshift.Events.Domain.Enums;
+using Qinshift.Events.Domain.Models;
+
+Console.WriteLine("========== SUPER MARKET ======");
+// Create a new market instance
+Market market = new Market();
+market.Name = "Super Market";
+market.ProductType = ProductType.Food;
+// Create a new user instance
+
+User user1 = new User { Id=1, Name="Bob Bobsky", Age=28, FavoriteProductType=ProductType.Food };
+User user3 = new User { Id=1, Name="Mile Popski", Age=22, FavoriteProductType=ProductType.Other };
+User user2 = new User { Id = 2, Name = "Jill Jilliski", Age = 18, FavoriteProductType = ProductType.Electronics };
+// Subscribe users for promotions
+market.SubscribeForPromotion(user1.ReadPromotion, "bob.bobsky@gmail.com");
+// Subscribe users for information
+market.SubscribeForInformation(user2.ReadInformation, "jill123@gmail.com");
+
+// Send promotions
+market.SendPromotions();
+
+
+Console.WriteLine("--------------------------");
+// Unsubscribe user1
+market.UnSubscribeForPromotions(user1.ReadPromotion, "I don't need food promotions anymore.");
+
+market.SubscribeForPromotion(user3.ReadPromotion, "mile.popski@gmail.com");
+market.SubscribeForPromotion(user2.ReadPromotion, "jill123@gmail.com");
+
+market.SendPromotions();
+
+
+Console.WriteLine("==== ZALBI I POPLAKI ====");
+market.ReadZalbiPoplaki(); 
+Console.ReadLine();
