4015828af9dede9801cd1238a6907f9ab0d3b1d4
tijanastojanovska <stojanovska_tijana@outlook.com>
2024-04-16 20:46:55 +0200

    Class 08

---
+++ g6/Class 08/Code/Events/Program.cs
@@ -0,0 +1,49 @@
+﻿using Events;
+
+User user = new User();
+user.FirstName = "Bob";
+user.Age = 30;
+user.FavouriteProductType = ProductTypeEnum.Fruit;
+user.FavouriteStore = StoreEnum.SportVision;
+
+User secondUser = new User();
+secondUser.FirstName = "Ana";
+secondUser.Age = 25;
+secondUser.FavouriteProductType = ProductTypeEnum.Cosmetics;
+secondUser.FavouriteStore = StoreEnum.Zara;
+
+Market market = new Market();
+market.Name = "Super market";
+market.Address = "Address 1";
+market.CurrentProductOnPromotion = ProductTypeEnum.Fruit;
+
+Mall mall = new Mall();
+mall.Name = "CityMall";
+mall.StoreOnSale = StoreEnum.Zara;
+
+//here we pass the method that will be called once the event happens
+//the method that we pass as an argument here must follow the PromotionSender delegate signature
+market.SubscribeForPromotion(user.ReactOnPromotion);
+
+//market.SubscribeForPromotion(user.SayHello); //ERROR -> SayHello does not follow the PromotionSender delegate signature
+
+market.SubscribeForPromotion(secondUser.ReactOnPromotion);
+
+market.SubscribeForPromotion(user.PrintMarketProduct); //we can subscribe multiple methods as long as they follow the deleagte signature
+
+//in this moment, for the first user ReactOnPromotion and PrintMarketProduct will be called, because they are both subscribed
+//for the second user only the ReactOnPromotion method will be called
+market.SendPromotion();
+
+//we remove PrintMarketProduct from user from subscribers of the event
+market.UnSubscribeForPromotion(user.PrintMarketProduct);
+
+//in this moment, for the first user ReactOnPromotion
+//for the second user only the ReactOnPromotion method will be called
+market.SendPromotion();
+
+mall.SubscribeForSale(user.ReactOnStoreSale);
+mall.SubscribeForSale(secondUser.ReactOnStoreSale);
+mall.SendSaleNotification();
+mall.UnSubscribeForSale(user.ReactOnStoreSale);
+mall.SendSaleNotification();
---
+++ g6/Class 08/Code/Events/User.cs
@@ -0,0 +1,52 @@
+﻿
+namespace Events
+{
+    public class User
+    {
+
+        public string FirstName { get; set; }   
+        public int Age { get; set; }
+
+        public ProductTypeEnum FavouriteProductType {  get; set; }
+
+        public StoreEnum FavouriteStore {  get; set; }
+
+        //subscription method, must follow the delagte signature
+        //it will be executed when certain product is on promotion
+        public void ReactOnPromotion(ProductTypeEnum productType)
+        {
+            if(productType == FavouriteProductType)
+            {
+                Console.WriteLine($"{FirstName}: My favourite product is on promotion");
+            }
+            else
+            {
+                Console.WriteLine($"{FirstName} must wait. {FavouriteProductType.ToString()} is not on promotion");
+            }
+        }
+
+
+        public void PrintMarketProduct(ProductTypeEnum productType)
+        {
+            Console.WriteLine($"The product that is on promotion at our market is {productType}");
+        }
+
+        //we need a method that follows the StoreSale delegate (returns string and has one param StoreEnum)
+        public string ReactOnStoreSale(StoreEnum store)
+        {
+            if (store == FavouriteStore)
+            {
+                return $"{FirstName}: My favourite store is on sale";
+            }
+            else
+            {
+                return $"{FirstName} must wait. {FavouriteStore.ToString()} is not on sale";
+            }
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
