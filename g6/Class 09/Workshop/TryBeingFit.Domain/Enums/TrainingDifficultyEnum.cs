1bf538778fb1f944f8de67efb0f36785cd8c8063
tijanastojanovska <stojanovska_tijana@outlook.com>
2024-04-18 20:46:44 +0200

    Class 09

---
+++ g6/Class 09/Workshop/TryBeingFit.Domain/Enums/TrainingDifficultyEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryBeingFit.Domain.Enums
+{
+    public enum TrainingDifficultyEnum
+    {
+        Easy =1,
+        Medium,
+        Hard
+    }
+}
