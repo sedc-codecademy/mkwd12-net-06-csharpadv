9384406fa09ba02e7aa7c814e9bd1784c23d70a2
tpelivanov <t.pelivanov@linkplus-it.com>
2024-04-18 20:41:15 +0200

    Class09 & workshop

---
+++ g7/Class09/Class09/Class09.FileStreams/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+string appPath = @"..\..\..\";
+string folderPath = appPath + @"myFolder\";
+string filePath = folderPath + @"test.txt";
+
+if(!Directory.Exists(folderPath))
+{
+    Directory.CreateDirectory(folderPath);
+    Console.WriteLine("New directory was created!");
+}
+
+//Writing with streamwriter
+using (StreamWriter sw = new StreamWriter(filePath))
+{
+    sw.WriteLine("Hello Quinshift!");
+    sw.WriteLine("We are writing text from StreamWriter!");
+    Console.WriteLine("Writing is complete!");
+}
+
+// Writing without rewriting the document with StreamWriter
+using (StreamWriter sw = new StreamWriter(filePath))
+{
+    sw.WriteLine("Hello from outer space!");
+    sw.WriteLine("We are writing text from a galaxy far far away!");
+    Console.WriteLine("Writing is complete!");
+}
+
+using (StreamWriter sw = new StreamWriter(filePath, true))
+{
+    sw.WriteLine("Hello Quinshift!");
+    sw.WriteLine("We are writing text from StreamWriter!");
+    Console.WriteLine("Writing is complete!");
+}
+
+Console.ReadLine();
+
+using(StreamReader sr = new StreamReader(filePath))
+{
+    string firstLine = sr.ReadLine();
+    string secondLine = sr.ReadLine();
+    string restContent = sr.ReadToEnd();
+    Console.WriteLine($"First Line {firstLine}");
+    Console.WriteLine($"Second Line {secondLine}");
+    Console.WriteLine("Rest of content in the file bellow");
+    Console.WriteLine($"{restContent}");
+}
---
+++ g7/Class09/Class09/LoggerExample/Program.cs
@@ -0,0 +1,47 @@
+﻿using LoggerExample.Entities;
+
+LoggerService logger = new LoggerService();
+List<User> users = new List<User>()
+{
+    new User(){Username = "Bob20", Password = "123456", Age = 20, Id = 4 },
+    new User(){Username = "JillCool", Password = "coolcat", Age = 34, Id = 12 },
+    new User(){Username = "Gregonator", Password = "astalavista", Age = 44, Id = 76 }
+};
+void Login(string username, string password)
+{
+    User user = users.Where(x => x.Username == username && x.Password == password).FirstOrDefault();
+    if (user == null) throw new InvalidLoginException(username);
+    Console.WriteLine($"Welcome {username}!");
+}
+
+try
+{
+    Console.WriteLine("Welcome to the amazing app!");
+    Console.WriteLine("Enter username:");
+    string username = Console.ReadLine();
+    Console.WriteLine("Enter password:");
+    string password = Console.ReadLine();
+    Login(username, password);
+
+}
+catch (InvalidLoginException ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine("Check your username and password and try again!");
+    logger.Log("InvalidLogin", ex.Message, ex.Username);
+}
+catch (InvalidUserPropertyException ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine("Try and enter a correct value please!");
+    logger.Log("InvalidUserProperty", ex.Message, ex.BrokenUser);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine("Please try again or contact support!");
+    logger.Log(ex.GetType().Name, ex.Message);
+    logger.LogError();
+}
+Console.WriteLine("Enjoy your day!");
+Console.ReadLine();
