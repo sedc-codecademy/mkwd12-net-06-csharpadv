285c6f68860be0a69b3d867527f4815077968326
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2024-04-20 12:33:49 +0200

    Class 10 finished

--- g2/Class10/Class10Demo/NullableTypes/Program.cs
+++ g2/Class10/Class10Demo/NullableTypes/Program.cs
@@ -3,8 +3,10 @@ using NullableTypes;
 
 ExtendedConsole.PrintInColor("\n================= NULLABLE TYPES =================\n\n", ConsoleColor.Cyan);
 
-//int number = null;
-int? number = null;
+// ===> VALUE types are NOT NULLABLE by default
+
+// int number = null; // won't work because int is not nullable by default
+int? number = null; // with the '?' we can make the non-nullable types to be nullable
 
 //double decimalNumber = null;
 double? decimalNumber = null;
@@ -14,11 +16,11 @@ bool? isTrue = null;
 
 DateTime? dateTime = null;
 
-
+// NOTE: string is nullable by default
 string text = null;
-string? text2 = null;
+string? text2 = null; // here we explicitly tell that this variable is nullable
 
-
+// ===> PERSON EXAMPLE
 Person person = null;
 
 Person bob = new Person();
@@ -26,4 +28,5 @@ Console.WriteLine(bob.Name); // null
 Console.WriteLine(bob.Id); // 0
 Console.WriteLine(bob.Score); // null
 Console.WriteLine(bob.IsEmployed); // false
+// bob.IsEmployed = null; // We can't put null in a bool
 Console.WriteLine(bob.HasPet); // null
