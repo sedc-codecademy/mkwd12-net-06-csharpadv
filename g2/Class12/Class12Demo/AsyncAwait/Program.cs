2806b46154031b1761790ae6546846887d40ee2f
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2024-04-25 19:36:57 +0200

    Class 12 Tasks

---
+++ g2/Class12/Class12Demo/AsyncAwait/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine("\n============== ASYNC/AWAIT ==============\n\n");
+Console.ResetColor();
+
+
+#region Methods
+// Synchronous method
+void SendMessage(string message)
+{
+    Console.WriteLine("Sending message ...\n");
+    Thread.Sleep(7000);
+    Console.WriteLine(string.Format(@"The message ""{0}"" was sent!", message));
+}
+
+void ShowAd(string product)
+{
+    Console.WriteLine("While you wait let us show you an ad:");
+    Console.Write("Buy the best product in the world ");
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.Write(product);
+    Console.ResetColor();
+    Console.Write(" now and get ");
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write("FREE");
+    Console.ResetColor();
+    Console.WriteLine(" shipping worldwide!\n");
+}
+
+#endregion
+
+string message = "Hello there Qinshift Academy students";
+
+// ===> Example for sync execution
+SendMessage(message);
+ShowAd("Laptop");
+
+Console.WriteLine("\nEverything finished...\n");
+Console.ReadLine();
---
+++ g2/Class12/Class12Demo/Tasks/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine("\n============== TASKS ==============\n\n");
+Console.ResetColor();
+
+
+Console.WriteLine("App start...\n");
+
+// ===> Single Task Example
+//Task myTask = new Task(() =>
+//{
+//    Thread.Sleep(2000);
+//    Console.WriteLine("Running after 2000ms");
+//});
+//myTask.Start();
+
+
+// ===> Single task with return type example
+//Task<int> valueTask = new Task<int>(() =>
+//{
+//    Thread.Sleep(2000);
+//    Console.WriteLine("We can now get the number...");
+//    return 3;
+//});
+//valueTask.Start();
+//Console.WriteLine(valueTask.Status);
+//Console.WriteLine(valueTask.Result);
+
+
+// ===> Creating Task directly on the Thread Pool
+//Task.Run(() =>
+//{
+//    Thread.Sleep(3000);
+//    Console.WriteLine("This is executed immediately...");
+//});
+
+
+
+for (int i = 1; i < 21; i++)
+{
+    int temp = i;
+    Task.Run(() =>
+    {
+        Thread.Sleep(1500);
+        Console.WriteLine($"TASK NO. {temp}");
+    });
+}
+
+
+Console.WriteLine("\nApp end...\n");
+
+Console.ReadLine();
