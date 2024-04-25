46767b4cea5134475e177f4a10af6f127ed2ca43
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2024-04-25 18:27:02 +0200

    Class 12 Threads

---
+++ g2/Class12/Class12Demo/Threads/Program.cs
@@ -0,0 +1,58 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine("\n============== THREADS ==============\n\n");
+Console.ResetColor();
+// Thread is a path of execution within a process
+// They are the smallest unit of execution that can be scheduled by an operating system
+// They allow a program to perform multiple tasks concurrently (at same time)
+
+#region Method
+// Synchronous
+void SendMessages()
+{
+    Console.WriteLine("Getting ready...");
+    Thread.Sleep(2000);
+    Console.WriteLine("First message arrived!");
+    Thread.Sleep(2000);
+    Console.WriteLine("Second message arrived!");
+    Thread.Sleep(2000);
+    Console.WriteLine("Third message arrived!");
+    Thread.Sleep(2000);
+    Console.WriteLine("All messages are recived!");
+    Console.ReadLine();
+}
+
+// Asynchronous
+void SendMessagesWithThreads()
+{
+    Console.WriteLine("Getting ready...");
+
+    Thread myThread = new Thread(() =>
+    {
+        Thread.Sleep(5000);
+        Console.WriteLine("First message arrived!");
+    });
+    myThread.Start();
+
+    new Thread(() =>
+    {
+        Thread.Sleep(3000);
+        Console.WriteLine("Second message arrived!");
+    }).Start();
+
+    new Thread(() =>
+    {
+        Thread.Sleep(1000);
+        Console.WriteLine("Third message arrived!");
+    }).Start();
+
+    Console.WriteLine("All messages are recived!");
+    Console.ReadLine();
+}
+
+#endregion
+
+// ===> Synchronous execution
+//SendMessages();
+
+// ===> Asynchronous execution
+SendMessagesWithThreads();
