9441e7eb98bb919d99a493c5ec2921020cc50921
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2024-04-25 20:57:14 +0200

    Class 12 Async/Await

--- g2/Class12/Class12Demo/AsyncAwait/Program.cs
+++ g2/Class12/Class12Demo/AsyncAwait/Program.cs
@@ -26,13 +26,77 @@ void ShowAd(string product)
     Console.WriteLine(" shipping worldwide!\n");
 }
 
+// Asynchronous method
+async Task SendMessageAsync(string message)
+{
+    Console.WriteLine("Sending message ...\n");
+    //Task.Run(() =>
+    await Task.Run(() =>
+    {
+        Thread.Sleep(7000);
+        Console.WriteLine($@"The message ""{message}"" was sent!");
+    });
+    Console.WriteLine("It takes just 7 seconds...");
+}
+
+async Task OurMainThread()
+{
+    await SendMessageAsync("Hello there Qinshift Academy students");
+    //string product = GetAdvertismentItem(); // null
+    string product = await GetAdvertismentItemAsync(); // Mouse => will await for the 'call' to finish
+    ShowAd(product);
+}
+
+string GetAdvertismentItem()
+{
+    string result = null;
+    Task.Run(() =>
+    {
+        // "call to db"
+        Thread.Sleep(7000);
+        result = "Mouse";
+    });
+    return result;
+}
+
+async Task<string> GetAdvertismentItemAsync()
+{
+    string result = null;
+    await Task.Run(() =>
+    {
+        // "call to db"
+        Thread.Sleep(7000);
+        result = "Mouse";
+    });
+    return result;
+}
+
 #endregion
 
 string message = "Hello there Qinshift Academy students";
 
 // ===> Example for sync execution
-SendMessage(message);
-ShowAd("Laptop");
+//SendMessage(message);
+//ShowAd("Laptop");
+
+// ===> Example of async execution
+//SendMessageAsync(message);
+//ShowAd("Potato");
+
+// ===> Example of sync execution of async operation
+//await SendMessageAsync(message);
+//ShowAd("Charger");
+
+// ===> we can assign the task to a variable
+//Task sendMessageTask = SendMessageAsync(message);
+//Console.WriteLine(sendMessageTask.Status);
+//ShowAd("Glasses");
+//Console.ReadLine();
+//Console.WriteLine(sendMessageTask.Status);
+
+
+OurMainThread();
+
 
 Console.WriteLine("\nEverything finished...\n");
 Console.ReadLine();
