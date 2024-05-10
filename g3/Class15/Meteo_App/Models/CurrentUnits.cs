fe938e4443ca27cd14b6304d3c00ca2483db2bfe
risto-panchevski <76558034+risto-panchevski@users.noreply.github.com>
2024-05-10 20:48:46 +0200

    Class 15

---
+++ g3/Class15/Meteo_App/Models/CurrentUnits.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Models
+{
+    public class CurrentUnits
+    {
+        [JsonProperty("temperature_2m")]
+        public string TemperatureUnit { get; set; }
+        [JsonProperty("wind_speed_10m")]
+        public string WindSpeedUnit { get; set; }
+    }
+}
---
+++ g3/Class15/Meteo_App/Services/MeteoService.cs
@@ -0,0 +1,24 @@
+﻿using Models;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Services
+{
+    public class MeteoService : IMeteoService
+    {
+        public async Task<WeatherModel> GetWeatherStat()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(
+                    "https://api.open-meteo.com/v1/forecast?latitude=41.99&longitude=21.42&current=temperature_2m,wind_speed_10m&hourly=temperature_2m");
+                response.EnsureSuccessStatusCode();
+                var result = await response.Content.ReadAsStringAsync();
+
+                var temperatureData = JsonConvert.DeserializeObject<WeatherModel>(result);
+
+                return temperatureData;
+            }
+        }
+    }
+}
