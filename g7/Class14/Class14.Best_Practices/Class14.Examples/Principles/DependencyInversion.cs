50a3b8fad7d221d0c0651a19edfd7367211d9227
tpelivanov <t.pelivanov@linkplus-it.com>
2024-05-11 12:43:45 +0200

    Good Practices and Principles part 2 and cSharpFeatures

--- g7/Class14/Class14.Best_Practices/Class14.Examples/Principles/DependencyInversion.cs
+++ g7/Class14/Class14.Best_Practices/Class14.Examples/Principles/DependencyInversion.cs
@@ -53,4 +53,60 @@
     }
 
     //good example
+    public interface IMessage
+    {
+        void SendMessage();
+    }
+
+    public class Email : IMessage
+    {
+        public string EmailAddress { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+
+        public void SendMessage()
+        {
+            // .. code that will send an email
+        }
+    }
+
+    public class Sms : IMessage
+    {
+        public string PhoneNumber { get; set; }
+        public string Message { get; set; }
+
+        public void SendMessage()
+        {
+            // ... code that will send a message
+        }
+    }
+
+    public class Facebook : IMessage
+    {
+        public string FacebookAccount { get; set; }
+        public string Message { get; set; }
+
+        public void SendMessage()
+        {
+            // ... code that will send a facebook message
+        }
+    }
+
+    public class NotificationGood
+    {
+        private ICollection<IMessage> _messages;
+
+        public NotificationGood(ICollection<IMessage> messages)
+        {
+            _messages = messages;
+        }
+
+        public void Send()
+        {
+            foreach (var message in _messages)
+            {
+                message.SendMessage();
+            }
+        }
+    }
 }
---
+++ g7/Class14/Class14.Best_Practices/Class14.Examples/Principles/OpenClose.cs
@@ -0,0 +1,137 @@
+﻿namespace Class14.Examples.Principles
+{
+    //bad example
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Academy { get; set; }
+        public int Grade { get; set; }
+        public bool HasProject { get; set; }
+        public int Homeworks { get; set; }
+
+        //initial implementation
+        // the client has only one type of student, code student
+        public bool CheckClassDay(string day)
+        {
+            if (day == "monday" || day == "friday") return true;
+            return false;
+
+            //if (Academy == "Code")
+            //{
+            //    if (day == "monday" || day == "friday") return true;
+            //    return false;
+            //}
+            ////after we add more students =>
+            //else if (Academy == "Design")
+            //{
+            //    if (day == "monday" || day == "wednesday" || day == "friday") return true;
+            //    return false;
+            //}
+            //else if (Academy == "Network")
+            //{
+            //    if (day == "monday" || day == "thursday" day == "friday") return true;
+            //    return false;
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("no such acaddemy");
+            //} 
+            //    return false;
+        }
+        
+    }
+
+    /// <summary>
+    /// ////////////////////////////////////////////////////////////////////////////////////////
+    /// </summary>
+    //good example
+    //this is the initial student class
+    public abstract class StudentGood
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public AcademyType Academy { get; set; }
+        public abstract bool CheckClassDay(string day);
+    }
+
+    // this is the first student type => code
+    public class CodeStudent : StudentGood
+    {
+        public bool HasProject { get; set; }
+        public CodeStudent()
+        {
+            Academy = AcademyType.Code;
+        }
+
+        public override bool CheckClassDay(string day)
+        {
+            if (day == "monday" || day == "friday") return true;
+            return false;
+        }
+    }
+
+    public class DesignStudent : StudentGood
+    {
+        public int Homeworks { get; set; }
+        public DesignStudent()
+        {
+            Academy = AcademyType.Design;
+        }
+
+        public override bool CheckClassDay(string day)
+        {
+            if (day == "monday" || day == "wednesday" || day == "friday") return true;
+            return false;
+        }
+    }
+
+    public class NetworkStudent : StudentGood
+    {
+        public int Grade { get; set; }
+        public NetworkStudent()
+        {
+            Academy = AcademyType.Network;
+        }
+
+        public override bool CheckClassDay(string day)
+        {
+            if (day == "monday" || day == "thursday" || day == "friday") return true;
+            return false;
+        }
+    }
+
+    //we can have as many types of students in the future as we need
+    //and we add them without changing anything in the implementation that is working
+    //before the need to extend with more student types
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public enum AcademyType
+    {
+        None, 
+        Code,
+        Design,
+        Network
+        //in the future we might extent this enum with as many types of academies as we want
+        //...Art
+        //...Science
+        //...
+    }
+}
